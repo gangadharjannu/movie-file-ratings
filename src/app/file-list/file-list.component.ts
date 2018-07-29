66d34e3a20de58bf8a82723df9fdb8c4b71db2b8
Gangadhar <gangadhar014@gmail.com>
2018-07-29 21:28:16 +0200

    Added file list component which will list all files

---
+++ src/app/file-list/file-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-list',
+  templateUrl: './file-list.component.html',
+  styleUrls: ['./file-list.component.css']
+})
+export class FileListComponent implements OnInit {
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
