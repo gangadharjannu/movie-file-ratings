749e2ab63724739795eaa7bd9946b15be5687f61
Gangadhar <gangadhar014@gmail.com>
2018-07-29 21:29:03 +0200

    loaded and imported all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
+
 import { AppComponent } from './app.component';
+import { FileListComponent } from './file-list/file-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
